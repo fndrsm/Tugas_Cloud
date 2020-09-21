f72f16f15a056ee76a06fa44c51b63143d7c0ac9
fndrsm <48046968+fndrsm@users.noreply.github.com>
2020-09-21 21:42:10 +0700

    Add files via upload

---
+++ Tugas_Cloud.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>TugasCloud.com</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  body {
+    font-family: 'Montserrat';font-size: 15px;
+}
+  .fakeimg {
+    height: 200px;
+    background: #aaa;
+  }
+  .dark-mode {
+  background-color: black;
+  color: white;
+  </style>
+</head>
+<body>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <h1>Aktivitas Bermanfaat Saat Work From Home</h1>
+  <p>Beberapa sudah saya coba, mungkin kalian juga bisa.</p> 
+</div>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">TugasCloud.com</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h2>About Me</h2>
+      <h5>Photo of me:</h5>
+      <div class="fndrsm.jpg">Fndrsm on instagram</div>
+      <p>Saya orangnya pemalu dan ini buat tugas cloud computing ya. Tapi coba, dark modenya nih dibawah.</p>
+      <button onclick="myFunction()">Switch themes</button>
+      <script>
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+</script>
+      <h3>Sekedar Info</h3>
+      <p>Kalo penting aja ya bolehlah kita chattingan.</p>
+      <ul class="nav nav-pills nav-stacked">
+        <li class="active"><a href="https://www.instagram.com/fndrsm/">Instagram</a></li>
+        <li class="active"><a href="www.wattpad.com/story/150076725-moss">Wattpad</a></li>
+        <li class="active"><a href="falahmeola@gmail.com">Email</a></li>
+      </ul>
+      <hr class="hidden-sm hidden-md hidden-lg">
+    </div>
+    <div class="col-sm-8">
+      <h2>BERCOCOK TANAM</h2>
+      <h5>Last Update, Sept 21, 2020</h5>
+      <img src="https://images.unsplash.com/photo-1530836369250-ef72a3f5cda8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="Person holding plants" width="500" height="333">
+      <i><p>Photo by Joshua Lanzarini on Unsplash</p></i>
+      <p>Apabila kamu mempunyai hobi yang sama seperti saya, yaitu bercocok tanam, maka tidak perlu susah hati jika ternyata saat ini kamu tidak punya lahan untuk menanam. Cobalah untuk menanam secara hidroponik, dimana media tanamnya sangat sederhana, yaitu kotak plastik, netpot, rockwool dan air. Kalian bisa menanam sayuran, dimana sayuran tersebut dapat kalian gunakan sendiri untuk dimakan. Hal ini juga menghemat pengeluaran dan efektif dengan kondisi sekarang, yaitu mengurangi kegiatan keluar rumah, termasuk kegiatan keluar rumah untuk membeli sayur.</p>
+      <br>
+      <h2>MENJAHIT</h2>
+      <h5>Last Update, Sept 21, 2020</h5>
+      <img src="https://images.unsplash.com/photo-1564532394920-883437daf280?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Sewing machine" width="500" height="333">
+      <i><p>Photo by Juan Jose Porta on Unsplash</p></i>
+      <p>Menjahit bukanlah hal yang susah, dimana saat ini juga sudah bisa dipelajari di youtube tentang cara menjadi penjahit dadakan. Coba saja kalian lihat di youtube, ada banyak cara untuk membuat baju, tas atau peralatan lainnya, dimana dengan hanya menjahit saja. 
+
+Pertama-tama, jika kalian masih pemula, pelajari dahulu cara menjahit dengan tangan, yaitu menjahit dengan jarum dan benang secara manual (tanpa menggunakan mesin jahit). 
+
+Apabila menjahit dengan tangan sudah kalian kuasai, maka kalian dapat beralih menjahit dengan menggunakan mesin jahit, bahkan saat ini mesin jahit sudah mudah digunakan dengan adanya dinamo untuk membantu memutar mesin jahit tanpa memutarnya dengan kaki, seperti mesin jahit jaman dahulu kala.</p>
+	<br>
+      <h2>BERMAIN GAME</h2>
+      <h5>Last Update, Sept 21, 2020</h5>
+      <img src="https://images.unsplash.com/photo-1534423861386-85a16f5d13fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="Sewing machine" width="500" height="333">
+      <i><p>Photo by Sean Do on Unsplash</p></i>
+      <p>Hal menyenangkan lainnya adalah bermain game. Iya, bermain game, selain menyenangkan dan dapat membuat kita tidak sadar waktu, saat ini game juga dapat menghasilkan uang. Jadi game saat ini bukanlah hal yang negatif, tetapi juga dapat menjadi lahan penghasilan. game yang sering dimainkan antara lain adalah  game online seperti point blank, counter strike, hingga game online android seperti PUBG Mobile.</p>
+      <br>
+      <h2>BERNYANYI</h2>
+      <h5>Last Update, Sept 21, 2020</h5>
+      <img src="https://images.unsplash.com/photo-1517230878791-4d28214057c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="Sewing machine" width="500" height="333">
+      <i><p>Photo by Elizeu Dias on Unsplash</p></i>
+      <p>Jika kalian dirumah secara terus-menerus, maka kalian akan jenuh dan bahkan stress. Maka dari itu bernyanyi dapat menjadi cara menghibur diri juga dapat menghibur orang lain, apabila nyanyian kalian dinyanyikan dengan baik dan disiarkan di media sosial. Jika kalian belum dapat bernyanyi dengan baik, inilah kesempatan kalian untuk berlatih bernyanyi dirumah lewat youtube maupun aplikasi untuk berkaraoke.
+
+Bagaimana, cukup bervariasi bukan ide-ide diatas untuk kalian terapkan dikala seperti ini. Yang penting hindari stress dan jalankan hidup efektif dan bermanfaat, agar kedepannya hidup kita jadi lebih baik.</p>
+	<br>
+    <p>Nah, gimana nih ges. Dari semua yang ada diatas saya udah lakuin semua. Dari mulai tanam pohon sampai main game. Tapi saya ga lupa sama kuliah ya. Tetap belajar sama ngerjain tugas, dan gak lupa sama kuliah online ya. Tetap semangat kuliahnya ya ges. Jaga kesehatan, jangan lupa cuci tangan sama pakai masker.</p>
+    <br><br><br>
+    <p>Sumber artikel dari <a href="https://www.kompasiana.com/yanti70738/5e7f11fe097f364d4a2d6183/bermanfaat-saat-work-from-home?page=all#section1">kompasiana.com</a></p>
+    </div>
+  </div>
+</div>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <p>&copy; 2020 Falah Meola Fenderian</p>
+</div>
+
+</body>
+</html>
