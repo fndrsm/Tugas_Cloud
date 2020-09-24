a039bfae13bd4e70d6f00f2299a72025f58629d2
fndrsm <48046968+fndrsm@users.noreply.github.com>
2020-09-24 21:19:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
     <div class="col-sm-4">
       <h2>About Me</h2>
       <h5>Photo of me:</h5>
-      <img src="fndrsm.jpg" alt="Fndrsm on instagram" class="img-circle" style="width:200px;height:200px;">
+      <img class="img-circle" src="fndrsm.jpg" alt="Fndrsm on instagram" style="width:200px;height:200px;">
       <h5>Nama : Falah Meola Fenderian</h5>
 	<h5>NIM : 11180910000040</h5>
 	    <p>Saya orangnya pemalu ehehehe. Ohh ya, ini buat tugas cloud computing ya. Saya gak ahli kalo bikin web. Tapi, coba dark modenya nih dibawah.</p>
@@ -75,13 +75,13 @@ function myFunction() {
     <div class="col-sm-8">
       <h2>BERCOCOK TANAM</h2>
       <h5>Last Update, Sept 22, 2020</h5>
-      <img src="https://images.unsplash.com/photo-1530836369250-ef72a3f5cda8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="Person holding plants" width="500" height="333">
+      <img class="img-responsive" src="https://images.unsplash.com/photo-1530836369250-ef72a3f5cda8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="Person holding plants" width="500" height="333">
       <i><p>Photo by Joshua Lanzarini on Unsplash</p></i>
       <p>Apabila kamu mempunyai hobi yang sama seperti saya, yaitu bercocok tanam, maka tidak perlu susah hati jika ternyata saat ini kamu tidak punya lahan untuk menanam. Cobalah untuk menanam secara hidroponik, dimana media tanamnya sangat sederhana, yaitu kotak plastik, netpot, rockwool dan air. Kalian bisa menanam sayuran, dimana sayuran tersebut dapat kalian gunakan sendiri untuk dimakan. Hal ini juga menghemat pengeluaran dan efektif dengan kondisi sekarang, yaitu mengurangi kegiatan keluar rumah, termasuk kegiatan keluar rumah untuk membeli sayur.</p>
       <br>
       <h2>MENJAHIT</h2>
       <h5>Last Update, Sept 22, 2020</h5>
-      <img src="https://images.unsplash.com/photo-1564532394920-883437daf280?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Sewing machine" width="500" height="333">
+      <img class="img-responsive" src="https://images.unsplash.com/photo-1564532394920-883437daf280?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Sewing machine" width="500" height="333">
       <i><p>Photo by Juan Jose Porta on Unsplash</p></i>
       <p>Menjahit bukanlah hal yang susah, dimana saat ini juga sudah bisa dipelajari di youtube tentang cara menjadi penjahit dadakan. Coba saja kalian lihat di youtube, ada banyak cara untuk membuat baju, tas atau peralatan lainnya, dimana dengan hanya menjahit saja. 
 
@@ -91,13 +91,13 @@ Apabila menjahit dengan tangan sudah kalian kuasai, maka kalian dapat beralih me
 	<br>
       <h2>BERMAIN GAME</h2>
       <h5>Last Update, Sept 22, 2020</h5>
-      <img src="https://images.unsplash.com/photo-1534423861386-85a16f5d13fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="Sewing machine" width="500" height="333">
+      <img class="img-responsive" src="https://images.unsplash.com/photo-1534423861386-85a16f5d13fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="Sewing machine" width="500" height="333">
       <i><p>Photo by Sean Do on Unsplash</p></i>
       <p>Hal menyenangkan lainnya adalah bermain game. Iya, bermain game, selain menyenangkan dan dapat membuat kita tidak sadar waktu, saat ini game juga dapat menghasilkan uang. Jadi game saat ini bukanlah hal yang negatif, tetapi juga dapat menjadi lahan penghasilan. game yang sering dimainkan antara lain adalah  game online seperti point blank, counter strike, hingga game online android seperti PUBG Mobile.</p>
       <br>
       <h2>BERNYANYI</h2>
       <h5>Last Update, Sept 22, 2020</h5>
-      <img src="https://images.unsplash.com/photo-1517230878791-4d28214057c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="Sewing machine" width="500" height="333">
+      <img class="img-responsive" src="https://images.unsplash.com/photo-1517230878791-4d28214057c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="Sewing machine" width="500" height="333">
       <i><p>Photo by Elizeu Dias on Unsplash</p></i>
       <p>Jika kalian dirumah secara terus-menerus, maka kalian akan jenuh dan bahkan stress. Maka dari itu bernyanyi dapat menjadi cara menghibur diri juga dapat menghibur orang lain, apabila nyanyian kalian dinyanyikan dengan baik dan disiarkan di media sosial. Jika kalian belum dapat bernyanyi dengan baik, inilah kesempatan kalian untuk berlatih bernyanyi dirumah lewat youtube maupun aplikasi untuk berkaraoke.
 
